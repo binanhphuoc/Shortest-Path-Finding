a18a4abb72cf2a04fe09a828240c9be55b5d8def
Phuoc Do <binanhphuoc@gmail.com>
2018-01-28 14:51:10 -0500

    Initial Commit

---
+++ UninformedSearch/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UninformedSearch
+//
+//  Created by Phuoc Do on 1/28/18.
+//  Copyright © 2018 Phuoc Do. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
